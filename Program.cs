556155713bda60bc82d743ad2d4ae3aaeb10558e
Obemik <misaperepecaj@gmail.com>
2024-11-15 23:58:08 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace homeworksCS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
