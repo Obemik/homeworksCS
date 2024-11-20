15be75dec1c9c4a8079e37dacb8d5f5a475d6d43
Obemik <misaperepecaj@gmail.com>
2024-11-20 16:54:34 +0200

    Ex 5

--- Program.cs
+++ Program.cs
@@ -1,10 +1,50 @@
-﻿namespace homeworksCS
+﻿using System;
+
+namespace ArithmeticExpression
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter an arithmetic expression with + and - operators:");
+            string input = Console.ReadLine();
+
+            input = input.Replace(" ", "");
+
+            int result = 0;
+            int currentNumber = 0;
+            char currentOperator = '+';
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char currentChar = input[i];
+
+                if (char.IsDigit(currentChar))
+                {
+                    currentNumber = currentNumber * 10 + (currentChar - '0'); 
+                }
+
+                if (i == input.Length - 1 || currentChar == '+' || currentChar == '-')
+                {
+                    if (currentOperator == '+')
+                    {
+                        result += currentNumber; 
+                    }
+                    else if (currentOperator == '-')
+                    {
+                        result -= currentNumber; 
+                    }
+
+                    if (currentChar == '+' || currentChar == '-')
+                    {
+                        currentOperator = currentChar; 
+                    }
+
+                    currentNumber = 0; 
+                }
+            }
+
+            Console.WriteLine($"Result: {result}");
         }
     }
 }
