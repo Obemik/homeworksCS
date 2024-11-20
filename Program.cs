d3103fd146bb8b31a78a8c7e1b4f49857384f871
Obemik <misaperepecaj@gmail.com>
2024-11-20 16:29:14 +0200

    Ex3

--- Program.cs
+++ Program.cs
@@ -1,10 +1,43 @@
-﻿namespace homeworksCS
+﻿using System;
+
+namespace CaesarCipher
 {
-    internal class Program
+    class Program
     {
+        static string Encrypt(string input, int shift)
+        {
+            char[] buffer = input.ToCharArray();
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                char letter = buffer[i];
+
+                if (char.IsLetter(letter))
+                {
+                    char d = char.IsUpper(letter) ? 'A' : 'a'; 
+                    buffer[i] = (char)((((letter + shift) - d) % 26 + 26) % 26 + d); 
+                }
+            }
+            return new string(buffer);
+        }
+
+        static string Decrypt(string input, int shift)
+        {
+            return Encrypt(input, -shift); 
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Enter the text to encrypt: ");
+            string text = Console.ReadLine();
+
+            Console.Write("Enter the shift (number): ");
+            int shift = int.Parse(Console.ReadLine());
+
+            string encryptedText = Encrypt(text, shift);
+            Console.WriteLine($"Encrypted text: {encryptedText}");
+
+            string decryptedText = Decrypt(encryptedText, shift);
+            Console.WriteLine($"Decrypted text: {decryptedText}");
         }
     }
 }
