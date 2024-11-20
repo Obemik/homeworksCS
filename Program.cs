f30c28a45728cb1fec93856728d3a143c70a087e
Obemik <misaperepecaj@gmail.com>
2024-11-20 17:23:51 +0200

    Ex7

--- Program.cs
+++ Program.cs
@@ -1,10 +1,36 @@
-﻿namespace homeworksCS
+﻿using System;
+
+namespace TextFilter
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter the text:");
+            string inputText = Console.ReadLine();
+
+            Console.WriteLine("Enter the inappropriate word:");
+            string inappropriateWord = Console.ReadLine();
+
+            int replacements = 0;
+
+            string resultText = ReplaceInappropriateWord(inputText, inappropriateWord, ref replacements);
+
+            Console.WriteLine("\nModified text:");
+            Console.WriteLine(resultText);
+            Console.WriteLine($"\nStatistics: {replacements} replacement(s) of the word '{inappropriateWord}'");
+        }
+
+        static string ReplaceInappropriateWord(string text, string word, ref int replacementCount)
+        {
+            string modifiedText = text;
+            while (modifiedText.Contains(word))
+            {
+                modifiedText = modifiedText.Replace(word, new string('*', word.Length));
+                replacementCount++; 
+            }
+
+            return modifiedText;
         }
     }
 }
