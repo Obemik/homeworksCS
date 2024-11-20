2ae29cca95c3a01c2d90bb7ec414820ff35a6c90
Obemik <misaperepecaj@gmail.com>
2024-11-20 15:40:10 +0200

    Ex 7 done

--- Program.cs
+++ Program.cs
@@ -1,10 +1,23 @@
-﻿namespace homeworksCS
+﻿using System;
+
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        Console.Write("Enter a sentence: ");
+        string sentence = Console.ReadLine();
+
+        string[] words = sentence.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+        for (int i = 0; i < words.Length; i++)
         {
-            Console.WriteLine("Hello, World!");
+            char[] wordArray = words[i].ToCharArray();
+            Array.Reverse(wordArray);
+            words[i] = new string(wordArray);
         }
+
+        string reversedSentence = string.Join(" ", words);
+
+        Console.WriteLine($"Reversed sentence: {reversedSentence}");
     }
 }
