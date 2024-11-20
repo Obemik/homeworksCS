5b6467f5be2a8293861386cb8938ae7b5ae113e5
Obemik <misaperepecaj@gmail.com>
2024-11-20 16:58:24 +0200

    Ez6

--- Program.cs
+++ Program.cs
@@ -1,10 +1,39 @@
-﻿namespace homeworksCS
+﻿using System;
+
+namespace SentenceCapitalizer
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter a text:");
+            string input = Console.ReadLine();
+
+            string result = CapitalizeFirstLetterOfEachSentence(input);
+
+            Console.WriteLine("Modified text:");
+            Console.WriteLine(result);
+        }
+
+        static string CapitalizeFirstLetterOfEachSentence(string text)
+        {
+            bool newSentence = true;
+            char[] chars = text.ToCharArray();
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (newSentence && char.IsLetter(chars[i]))
+                {
+                    chars[i] = char.ToUpper(chars[i]); 
+                    newSentence = false;
+                }
+                else if (chars[i] == '.' || chars[i] == '!' || chars[i] == '?')
+                {
+                    newSentence = true; 
+                }
+            }
+
+            return new string(chars);
         }
     }
 }
