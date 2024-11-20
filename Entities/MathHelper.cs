1db154af7cb169e45dbc4dab05b797e70d8fc438
Obemik <misaperepecaj@gmail.com>
2024-11-20 18:01:32 +0200

    Ex1

---
+++ Entities/MathHelper.cs
@@ -0,0 +1,23 @@
+﻿namespace ClassApp
+{
+    internal static class MathHelper
+    {
+        public static int GetProductInRange(int start, int end)
+        {
+            if (start > end)
+            {
+                int temp = start;
+                start = end;
+                end = temp;
+            }
+
+            int product = 1;
+            for (int i = start; i <= end; i++)
+            {
+                product *= i;
+            }
+
+            return product;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,23 @@
-﻿namespace homeworksCS
+﻿using System;
+using System.Text;
+
+namespace ClassApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            try
+            {
+                int product = MathHelper.GetProductInRange(1, 5);
+                Console.WriteLine($"Добуток чисел в діапазоні: {product}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Виникла помилка: {ex.Message}");
+            }
         }
     }
 }
