6e84278144daf26384f3a2658b5d3847d13a1802
Obemik <misaperepecaj@gmail.com>
2024-11-23 14:17:46 +0200

    Lab ex6

---
+++ Enteties/Airplane.cs
@@ -0,0 +1,39 @@
+﻿namespace ClassApp.Entities
+{
+    internal class Airplane
+    {
+        private string name;
+        private string manufacturer;
+        private int year;
+        private string airplaneType;
+
+        public Airplane(string name, string manufacturer, int year, string airplaneType)
+        {
+            this.name = name;
+            this.manufacturer = manufacturer;
+            this.year = year;
+            this.airplaneType = airplaneType;
+        }
+
+        public void DisplayData()
+        {
+            Console.WriteLine($"Назва літака: {name}");
+            Console.WriteLine($"Виробник: {manufacturer}");
+            Console.WriteLine($"Рік випуску: {year}");
+            Console.WriteLine($"Тип літака: {airplaneType}");
+        }
+
+        public void DisplayData(bool shortVersion)
+        {
+            if (shortVersion)
+            {
+                Console.WriteLine($"Назва літака: {name}");
+                Console.WriteLine($"Тип літака: {airplaneType}"); 
+            }
+            else
+            {
+                DisplayData(); 
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,40 @@
-﻿namespace homeworksCS
+﻿using ClassApp.Entities;
+using System.Text;
+
+namespace ClassApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            try
+            {
+                Console.WriteLine("Введіть назву літака:");
+                string name = Console.ReadLine();
+
+                Console.WriteLine("Введіть назву виробника:");
+                string manufacturer = Console.ReadLine();
+
+                Console.WriteLine("Введіть рік випуску:");
+                int year = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Введіть тип літака:");
+                string airplaneType = Console.ReadLine();
+
+                Airplane airplane = new Airplane(name, manufacturer, year, airplaneType);
+
+                Console.WriteLine("\nІнформація про літак:");
+                airplane.DisplayData();
+
+                Console.WriteLine("\nСкорочена інформація про літак:");
+                airplane.DisplayData(true);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Помилка: {ex.Message}");
+            }
         }
     }
 }
