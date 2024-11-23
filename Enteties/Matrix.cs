28fdf56262a9201307bb645b6d22db9ee151e92a
Obemik <misaperepecaj@gmail.com>
2024-11-23 14:47:52 +0200

    Lab ex7

---
+++ Enteties/Matrix.cs
@@ -0,0 +1,90 @@
+﻿namespace ClassApp.Entities
+{
+    internal class Matrix
+    {
+        private int[,] data;
+        private int rows;
+        private int columns;
+
+        public Matrix()
+        {
+            rows = 0;
+            columns = 0;
+            data = new int[0, 0];
+        }
+
+        public Matrix(int rows, int columns)
+        {
+            this.rows = rows;
+            this.columns = columns;
+            data = new int[rows, columns];
+        }
+
+        public Matrix(int[,] array)
+        {
+            rows = array.GetLength(0);
+            columns = array.GetLength(1);
+            data = array;
+        }
+
+        public void InputData()
+        {
+            Console.WriteLine("Введіть елементи матриці:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"Елемент [{i},{j}]: ");
+                    data[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+        }
+
+        public void InputDataRandom(int minValue, int maxValue)
+        {
+            Random random = new Random();
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    data[i, j] = random.Next(minValue, maxValue + 1);
+                }
+            }
+        }
+
+        public void DisplayMatrix()
+        {
+            Console.WriteLine("Матриця:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{data[i, j],5}");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public int GetMinimum()
+        {
+            int min = data[0, 0];
+            foreach (int value in data)
+            {
+                if (value < min)
+                    min = value;
+            }
+            return min;
+        }
+
+        public int GetMaximum()
+        {
+            int max = data[0, 0];
+            foreach (int value in data)
+            {
+                if (value > max)
+                    max = value;
+            }
+            return max;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,54 @@
-﻿namespace homeworksCS
+﻿using ClassApp.Entities;
+
+namespace ClassApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            try
+            {
+                Console.WriteLine("Введіть кількість рядків матриці:");
+                int rows = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введіть кількість стовпців матриці:");
+                int columns = int.Parse(Console.ReadLine());
+
+                Matrix matrix = new Matrix(rows, columns);
+
+                Console.WriteLine("Як заповнити матрицю?");
+                Console.WriteLine("1 - Випадкові числа");
+                Console.WriteLine("2 - Ввести вручну");
+                int choice = int.Parse(Console.ReadLine());
+
+                if (choice == 1)
+                {
+                    Console.WriteLine("Введіть мінімальне значення:");
+                    int minValue = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Введіть максимальне значення:");
+                    int maxValue = int.Parse(Console.ReadLine());
+
+                    matrix.InputDataRandom(minValue, maxValue);
+                }
+                else if (choice == 2)
+                {
+                    matrix.InputData();
+                }
+                else
+                {
+                    Console.WriteLine("Неправильний вибір.");
+                    return;
+                }
+
+                Console.WriteLine("\nМатриця:");
+                matrix.DisplayMatrix();
+
+                Console.WriteLine($"\nМінімальний елемент: {matrix.GetMinimum()}");
+                Console.WriteLine($"Максимальний елемент: {matrix.GetMaximum()}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Помилка: {ex.Message}");
+            }
         }
     }
 }
